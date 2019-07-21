09f582a5babec1c43b791b63638fe13a0547142c
Kevin Huang <thekevinhuang@gmail.com>
2019-07-21 10:54:19 -0400

    adding react home

--- Simptom/client/src/App.js
+++ Simptom/client/src/App.js
@@ -1,18 +1,40 @@
 import React, {Component} from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import Home from './components/general/Home'
 
 import './App.css';
 
 class App extends Component {
   constructor() {
     super()
+    this.state = {
+      loggedin: false,
+      currentUser = ''
+    }
+  }
+
+  componentDidMount() {
+    if(typeof localStorage === 'object'){
+      try {
+        const currentUser = localStorage.getItem('current_user')
+        this.setState({
+          currentUser: currentUser
+        })
+      } catch(e) {
+        alert('App error')
+      }
+    }
   }
 
   render() {
     return (
       <Router>
-
+        <Switch>
+          <Route exact path="/" component={}/>
+        </Switch>
       </Router>
     )
   }
 }
+
+export default App
